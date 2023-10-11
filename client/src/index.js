ef6c2f66b4a8e24d286a423b621775b2a5d81b50
hamisirizwan <hamisirizwan5@gmail.com>
2023-10-11 20:24:49 +0300

    changed to backend endpoints

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+// axios.defaults.baseURL = "http://localhost:5000/api";
 
-// axios.defaults.baseURL = "https://class-eight-api.vercel.app/api";
+axios.defaults.baseURL = "https://class-eight-api.vercel.app/api";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
